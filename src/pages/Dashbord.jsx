9e2efd1875ced8418d6308e3e63a32e9cecf3c4b
sdetai <aigulidrisova8@gmail.com>
2023-12-08 15:21:17 -0500

    first commit

---
+++ src/pages/Dashbord.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import {stocks} from  '../data'
+import { Link } from "react-router-dom"
+
+function  Dashbord()  {
+  //const[stocks,setStocks]=useState([])
+ 
+    console.log(stocks)
+  return (
+     <div className='container'>
+          <h1 >Most Active Stocks</h1>
+    <div className='stocks'>
+
+      
+        <div>
+        <h2>Company Name</h2>
+        {stocks.map((stock)=>{
+        const{name,symbol}=stock;
+        return(
+           
+            
+            <Link key ={symbol} to={'/stocks/'+symbol}>
+        
+            <h4>{`${name} (${symbol})`}</h4>
+            </Link>
+           
+        )
+    })} </div> 
+      <div>
+        <h2>Price</h2>
+        {stocks.map((stock)=>{
+        const{lastPrice}=stock;
+        return( 
+        
+            <h4>{lastPrice}</h4>
+          
+           
+        )
+    })} </div> 
+    
+    <div>
+        <h2>Change</h2>
+        {stocks.map((stock)=>{
+        const{change}=stock;
+        return( 
+        
+            <h4>{change}</h4>
+          
+           
+        )
+    })} </div> 
+    </div>
+    </div>
+  )
+}
+
+export default Dashbord
